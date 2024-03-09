0a62dcef1e08908d8e3b278635757f154bfd030b
Alex Maslov <upzzdl@Alexs-MacBook-Air.local>
2024-03-09 21:29:36 +0300

    form correction &&  templates

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, Form, Depends
 from fastapi.responses import HTMLResponse, RedirectResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -7,12 +7,16 @@ from datetime import datetime
 from sqlmodel import SQLModel, Field, create_engine
 
 from typing import Annotated, Optional
-from model.point import POINT
+from model.point import Point
 from model.market import Market
 
 app = FastAPI(debug=True)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
+db = []
+for item in Point:
+    print(f" Name { item.name}, \t\tvalue {item.value}")
 
 
 #  Table=true adds Deal definition to SQLModel.metadata
@@ -35,14 +39,27 @@ class Deal(SQLModel, table=True):
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("form.html", {"request": request})
+    return templates.TemplateResponse(
+        name="form.html", request=request, context={"choices": Point}
+    )
 
 
 @app.post("/db")
-async def form(base: Annotated[str, Form]):
-    print(base)
-    return {"base": base}
-    # return RedirectResponse("/index", status_code=303)
+async def form(
+    base: str = Form(...),
+    base_amount: float = Form(...),
+    dest: str = Form(...),
+    dest_amount: float = Form(...),
+):
+    obj = Deal(
+        base=base,
+        dest=dest,
+        base_amount=base_amount,
+        dest_amount=dest_amount,
+    )
+    db.append(obj)
+    print()
+    return RedirectResponse("/", status_code=303)
 
 
 @app.get("/items/{id}", response_class=HTMLResponse)
--- src/model/point.py
+++ src/model/point.py
@@ -1,4 +1,5 @@
 from typing import Literal
+from enum import Enum, auto
 
 # https://youtube.com/shorts/8PqbChgNDcw?si=88jjV9yuwMnr9x5I
 POINT = Literal[
@@ -16,6 +17,23 @@ POINT = Literal[
     "MATIC",
     "REN",
 ]
+
+
+class Point(Enum):
+    USDT = auto()
+    BTC = auto()
+    LTC = auto()
+    ADA = auto()
+    ATOM = auto()
+    ALGO = auto()
+    DOGE = auto()
+    XRP = auto()
+    BCH = auto()
+    DOT = auto()
+    FIL = auto()
+    MATIC = auto()
+    REN = auto()
+
 
 RELATION: dict[POINT, list[POINT]] = {
     ## dict[str:set]
